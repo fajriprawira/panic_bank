47d5b858c527bc799e0097ac7be785dc9004698e
Fajri Prawira <fajri.prawira95@gmail.com>
2024-07-18 12:28:20 +0700

    25% selesai

---
+++ migrations/20240717185332-create-profile.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Profiles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      fullname: {
+        type: Sequelize.STRING
+      },
+      dateofbirth: {
+        type: Sequelize.STRING
+      },
+      addres: {
+        type: Sequelize.STRING
+      },
+      phonenumber: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Profiles');
+  }
+};
